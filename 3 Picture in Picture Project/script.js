b639c9cfd777a004c90891fad70a49542d60e793
Syed Shawaiz <Shawai8009@gmail.com>
2025-11-08 20:53:46 +0500

    3rd Project in Progress but Error

---
+++ 3 Picture in Picture Project/script.js
@@ -0,0 +1,21 @@
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
+
+// Prompt User to Select a Media Stream and Pass that to Viedo element
+
+async function selectMediaStream()
+{
+    try
+    {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () =>{
+            videoElement.play();
+        }
+    }
+    catch(error)
+    {
+        // Catch Errors Here
+        console.log("Error here : " , error);
+    }
+}
