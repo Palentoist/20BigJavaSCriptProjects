004a324dcb565c62eacaf3761ccbe5857610b1ab
Syed Shawaiz <Shawai8009@gmail.com>
2025-11-20 11:52:55 +0500

    Setting up Local Storage in Task 8 and Pushing Final Version

--- 8 Custom Countdown/script.js
+++ 8 Custom Countdown/script.js
@@ -16,6 +16,7 @@ let countdownTitle = "";
 let countdownDate = "";
 let countdownValue = Date;
 let countdownActive;
+let savedCountdown;
 
 const second = 1000;
 const minute = second * 60;
@@ -69,6 +70,11 @@ function updateCountdown(e)
     e.preventDefault();
     countdownTitle = e.srcElement[0].value;
     countdownDate = e.srcElement[1].value;
+    savedCountdown = {
+        title : countdownTitle,
+        date : countdownDate,
+    };
+    localStorage.setItem('countdown' , JSON.stringify(savedCountdown));
 
     // Check for Valid Date
     if(countdownDate === '')
@@ -97,9 +103,27 @@ function reset()
     // Reset Values
     countdownTitle = '';
     countdownDate = ''; 
+    localStorage.removeItem('countdown');
+}
+
+function restorePreviousCountdown()
+{
+    // Get Countdown from local storage if availavle
+    if(localStorage.getItem('countdown'))
+    {
+        inputContianer.hidden = true;
+        savedCountdown = JSON.parse(localStorage.getItem('countdown'));
+        countdownTitle = savedCountdown.title;
+        countdownDate = savedCountdown.date;
+        countdownValue = new Date(countdownDate).getTime();
+        updateDOM();
+    }
 }
 
 // Event Listeners
 countdownForm.addEventListener('submit', updateCountdown);
 countdownBtn.addEventListener('click' , reset);
 completeBtn.addEventListener('click' , reset);
+
+// On Load , Check Local Storage
+restorePreviousCountdown();
